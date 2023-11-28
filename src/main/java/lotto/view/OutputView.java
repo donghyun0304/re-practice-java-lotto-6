6764728a98e2847c5e426aa787f8e7456eb8a7fa
donghyunkim <ghffu405@gmail.com>
2023-11-28 15:32:20 +0900

    feat: 로또번호 출력 메소드 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,4 +1,26 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+import lotto.dto.LottosDto;
+
+import javax.swing.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
+
+    public void printLottos(LottosDto lottosDto){
+        List<Lotto> lottos = lottosDto.getLottos();
+        for(Lotto lotto : lottos){
+            List<Integer> numbers = lotto.getNumbers();
+            String result = numbers.stream()
+                    .map(String::valueOf)
+                    .collect(Collectors.joining(", "));
+            System.out.println("[" + result + "]");
+        }
+    }
+
+    private void printFrontSquare(){
+        System.out.println();
+    }
 }
