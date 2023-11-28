0e4e267d31988ec579f62779aa02c892262e9e1c
donghyunkim <ghffu405@gmail.com>
2023-11-28 15:12:11 +0900

    feat: 로또생성 메소드 추가

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
+import lotto.utils.RandomNumberGenerator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class LottoMachine {
+
+    public List<Lotto> createLottos(int price){
+        // price 가격검증 로직 추가
+        int amount = price / 1000;
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i=0; i<amount; i++){
+            List<Integer> numbers = RandomNumberGenerator.createNumbers();
+            Collections.sort(numbers);
+            lottos.add(new Lotto(numbers));
+        }
+        return lottos;
+    }
 }
