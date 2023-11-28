7ce5b2b891b5cfd28d669864e2196b69056ef03d
donghyunkim <ghffu405@gmail.com>
2023-11-28 16:50:08 +0900

    feat: 보너스번호 매치 메소드 추가

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -3,7 +3,7 @@ package lotto.domain;
 import java.util.Collections;
 import java.util.List;
 
-public class Lotto {
+public class Lotto implements Comparable<Lotto>{
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -21,11 +21,28 @@ public class Lotto {
         return Collections.unmodifiableList(numbers);
     }
 
+    public boolean hasMatchedBonusNumbers(BonusNumber bonusNumber){
+        return numbers.contains(bonusNumber);
+    }
+
     @Override
     public String toString() {
         return "Lotto{" +
                 "numbers=" + numbers +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Lotto o) {
+        int count = 0;
+        for(Integer number : this.numbers){
+            for(Integer parameterNumber : o.numbers){
+                if(number == parameterNumber){
+                    count++;
+                }
+            }
+        }
+        return count;
     }
 
     // TODO: 추가 기능 구현
