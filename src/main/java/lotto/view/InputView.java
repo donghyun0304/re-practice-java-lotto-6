cf16a7d6361d26b0dbe243867193fb1eae4bac78
donghyunkim <ghffu405@gmail.com>
2023-11-28 14:48:53 +0900

    feat: 가격입력 메소드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,29 @@
 package lotto.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+import java.util.function.Supplier;
+
 public class InputView {
 
+    public int inputPrice(){
+        return getInput(() -> {
+            System.out.println("구입금액을 입력해 주세요.");
+            String input = Console.readLine();
+            // 검증로직 추가
+            return Integer.parseInt(input);
+        });
+    }
+
+    private <T> T getInput(Supplier<T> inputSupplier){
+        while(true){
+            try{
+                return inputSupplier.get();
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 
 }
