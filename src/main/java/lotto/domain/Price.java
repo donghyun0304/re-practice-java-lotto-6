8b83f6d6045fa86756f137f31ca018d5f2f62489
donghyunkim <ghffu405@gmail.com>
2023-11-29 17:55:06 +0900

    refactor: final 키워드 추가ㅣ

--- src/main/java/lotto/domain/Price.java
+++ src/main/java/lotto/domain/Price.java
@@ -5,12 +5,12 @@ public class Price {
     private static final int UNIT = 1000;
     private final int amount;
 
-    public Price(int amount) {
+    public Price(final int amount) {
         validateMultipleOfUnit(amount);
         this.amount = amount;
     }
 
-    private void validateMultipleOfUnit(int amount){
+    private void validateMultipleOfUnit(final int amount){
         if((amount % UNIT) != 0){
             throw new IllegalArgumentException("[ERROR] 1000 단위가 아닙니다.");
         }
