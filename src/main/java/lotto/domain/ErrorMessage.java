d2173189815f111cb6b8bf51a063587ded6cdf96
donghyunkim <ghffu405@gmail.com>
2023-11-29 17:25:28 +0900

    feat: 0보다 큰 숫자인지 검증

--- src/main/java/lotto/domain/ErrorMessage.java
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -3,4 +3,5 @@ package lotto.domain;
 public class ErrorMessage {
 
     public static final String NOT_ALL_NUMERIC = "[ERROR] 숫자로만 이루어져 있지 않습니다.";
+    public static final String NOT_OVER_ZERO = "[ERROR] 0보다 큰 숫자가 아닙니다.";
 }
--- src/main/java/lotto/validation/InputValidator.java
+++ src/main/java/lotto/validation/InputValidator.java
@@ -5,10 +5,20 @@ import lotto.domain.RegexPattern;
 
 public class InputValidator {
 
+    // 숫자로만 이루어져있는지 검증
     public static void validateOnlyNumbers(String input){
         if(!RegexPattern.ONLY_NUMBER.matches(input)){
             throw new IllegalArgumentException(ErrorMessage.NOT_ALL_NUMERIC);
         }
     }
+    
+    // 0보다 큰 숫자인지 검증
+    public static void validateOverZeroNumber(String input){
+        if(Integer.parseInt(input) <= 0){
+            throw new IllegalArgumentException(ErrorMessage.NOT_OVER_ZERO);
+        }
+    }
+    
+    
 
 }
