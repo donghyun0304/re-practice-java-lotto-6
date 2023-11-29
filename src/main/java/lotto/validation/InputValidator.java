4cf123994c85aa0ef5ffe9e08b949e00677ee92b
donghyunkim <ghffu405@gmail.com>
2023-11-29 20:58:42 +0900

    refactor: 검증 코드 로직 변경

--- src/main/java/lotto/validation/InputValidator.java
+++ src/main/java/lotto/validation/InputValidator.java
@@ -3,11 +3,22 @@ package lotto.validation;
 import lotto.domain.ErrorMessage;
 import lotto.domain.RegexPattern;
 
+import java.util.List;
+
 public class InputValidator {
 
     // 숫자로만 이루어져있는지 검증
     public static void validateOnlyNumber(String input){
         if(!RegexPattern.ONLY_NUMBER.matches(input)){
+            throw new IllegalArgumentException(ErrorMessage.NOT_ALL_NUMERIC);
+        }
+    }
+
+    //
+    public static void validateOnlyNumbers(List<String> input){
+        boolean hasOnlyNumbers = input.stream()
+                .allMatch(str -> RegexPattern.ONLY_NUMBER.matches(str));
+        if(!hasOnlyNumbers){
             throw new IllegalArgumentException(ErrorMessage.NOT_ALL_NUMERIC);
         }
     }
@@ -18,12 +29,14 @@ public class InputValidator {
             throw new IllegalArgumentException(ErrorMessage.NOT_OVER_ZERO);
         }
     }
-
+    
+    //컴마가 들어있는지 검증
     public static void validateIncludeComma(String input){
         if(RegexPattern.NOT_INCLUDE_COMMA.matches(input)){
             throw new IllegalArgumentException(ErrorMessage.NOT_INCLUDE_COMMA);
         }
     }
+    
     
     
 
