4b313591ce8df7815134c197bde669a06ea94e6e
donghyunkim <ghffu405@gmail.com>
2023-11-29 20:25:02 +0900

    feat: 에러메세지 추가

--- src/main/java/lotto/domain/ErrorMessage.java
+++ src/main/java/lotto/domain/ErrorMessage.java
@@ -5,4 +5,6 @@ public class ErrorMessage {
     public static final String NOT_ALL_NUMERIC = "[ERROR] 숫자로만 이루어져 있지 않습니다.";
     public static final String NOT_OVER_ZERO = "[ERROR] 0보다 큰 숫자가 아닙니다.";
     public static final String NOT_INCLUDE_COMMA = "[ERROR] , 가 포함되어 있지 않습니다.";
+    public static final String NOT_VALID_NUMBER_RANGE = "[ERROR] 정해진 범위의 숫자가 아닙니다.";
+    public static final String HAS_DUPLICATE = "[ERROR] 중복이 존재합니다.";
 }
