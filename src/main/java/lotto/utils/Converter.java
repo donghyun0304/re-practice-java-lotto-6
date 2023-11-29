ba24c8e53ad303144f72d91280c482676ba83cac
donghyunkim <ghffu405@gmail.com>
2023-11-29 20:50:59 +0900

    fix: static 추가

--- src/main/java/lotto/utils/Converter.java
+++ src/main/java/lotto/utils/Converter.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class Converter {
 
-    public List<String> parseToStringListByDelimiter(final String str, final String delimiter){
+    public static List<String> parseToStringListByDelimiter(final String str, final String delimiter){
         return Arrays.stream(str.split(delimiter))
                 .collect(Collectors.toList());
     }
