1f4f425448ec23135c967eb3f3a32ab9b7a2f7e4
donghyunkim <ghffu405@gmail.com>
2023-11-30 18:37:16 +0900

    refactor: 컨트롤러 리팩토링

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -21,26 +21,38 @@ public class LottoController {
         this.outputView = outputView;
     }
 
-    public void run(){
+    public void run() {
         LottoMachine lottoMachine = new LottoMachine();
+        List<Lotto> lottos = purchaseLottos(lottoMachine);
+
+        WinningLotto winningLotto = getWinningLotto();
+        displayResults(lottoMachine, lottos, winningLotto);
+    }
+
+    private List<Lotto> purchaseLottos(LottoMachine lottoMachine) {
         Price price = inputView.inputPrice();
         List<Lotto> lottos = lottoMachine.createLottos(price);
         LottosDto lottosDto = new LottosDto(lottos);
         outputView.printLottos(lottosDto);
+        return lottos;
+    }
 
+    private WinningLotto getWinningLotto() {
         Lotto winningNumbers = inputView.inputWinningNumbers();
-        WinningLotto winningLotto = getWinningLottoWhenGetValidBonus(winningNumbers);
-        Result result = lottoMachine.determineLottoResults(winningLotto, lottos);
+        return getWinningLottoWhenGetValidBonus(winningNumbers);
+    }
 
+    private void displayResults(LottoMachine lottoMachine, List<Lotto> lottos, WinningLotto winningLotto) {
+        Result result = lottoMachine.determineLottoResults(winningLotto, lottos);
         outputView.printLottoResult(result);
         outputView.printRateOfReturn(result.calcRateOfReturn());
     }
 
     private WinningLotto getWinningLottoWhenGetValidBonus(Lotto winningNumbers){
-        try{
+        try {
             int bonusNumber = inputView.inputBonusNumber();
             return WinningLotto.from(winningNumbers, bonusNumber);
-        }catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 로또번호와 중복된 번호가 존재합니다.");
             return getWinningLottoWhenGetValidBonus(winningNumbers);
         }
