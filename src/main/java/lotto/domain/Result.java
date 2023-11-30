a8506e1b50efbaac2e39c94f19d1146d93e0b656
donghyunkim <ghffu405@gmail.com>
2023-11-30 18:33:11 +0900

    feat: 메소드 분리

--- src/main/java/lotto/domain/Result.java
+++ src/main/java/lotto/domain/Result.java
@@ -16,10 +16,25 @@ public class Result {
 
     public double calcRateOfReturn(){
         Set<Map.Entry<Rank, Integer>> entries = result.entrySet();
-        double sumOfWinningAmount = entries.stream()
-                .mapToDouble(entry -> entry.getKey().getWinningAmount() * entry.getValue())
-                .sum();
-        double rateOfReturn = sumOfWinningAmount / (result.size() * 1000) * 100;
+        double sumOfWinningAmount = getSumOfWinningAmount(entries);
+        return getRateOfReturn(sumOfWinningAmount);
+    }
+
+    private double getRateOfReturn(double sumOfWinningAmount) {
+        double rateOfReturn = sumOfWinningAmount / (getSumOfResultValue() * 1000) * 100;
         return Math.round(rateOfReturn * 100) / 100.0;
     }
+
+    private int getSumOfResultValue(){
+        return result.entrySet().stream()
+                .mapToInt(entry -> entry.getValue())
+                .sum();
+    }
+
+    private double getSumOfWinningAmount(Set<Map.Entry<Rank, Integer>> entries) {
+        return entries.stream()
+                .mapToDouble(entry -> entry.getKey().getWinningAmount() * entry.getValue())
+                .sum();
+    }
+
 }
